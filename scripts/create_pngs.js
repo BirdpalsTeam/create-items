f060f00075663ca0683a7f237d011f397022cac6
Gasegamer <66225865+Gasegamer@users.noreply.github.com>
2022-04-23 17:19:16 -0300

    Made it compatible with items that doesn't have 6 sprites.

--- scripts/create_pngs.js
+++ scripts/create_pngs.js
@@ -1,21 +1,23 @@
 exports.start = async (item, path, PSD, psd)=>{
 	return await new Promise((resolve, reject) =>{
 		psd.parse();
+    let imagesSaved = 0;
 		let totalImages = 0;
 		psd.tree().descendants().forEach((node) => {
 			if(node.name == 'Item'){
+        totalImages = node._children.length;
 				node._children.forEach((layer) =>{
 					PSD.open(`${path}/${item}/${item}.psd`).then(function (psd) {
 							return layer.layer.image.saveAsPng(`${path}/${item}_${layer.layer.name}.png`);
 						}).then(function () {
 						console.log(`Saved ${item}_${layer.layer.name}.png at ${path}`);
-						totalImages += 1;
+						imagesSaved += 1;
 					})
 				})
 			}
 		});
 		function waitImage(){
-			if(totalImages == 6){
+			if(imagesSaved === totalImages){
 				setTimeout(()=>{
 					resolve('Finished!');
 				}, 3000)
